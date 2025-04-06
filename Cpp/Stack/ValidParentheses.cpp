01e35191b91f754a684622e98b021ab700b84932
Michael Akzam <michaelakzam@gmail.com>
2025-04-06 11:31:49 -0400

    Implemented ValidParentheses with stack and string implementation

--- Cpp/Stack/ValidParentheses.cpp
+++ Cpp/Stack/ValidParentheses.cpp
@@ -12,5 +12,38 @@ Complexity
 
 */
 
-#include <vector>
-#include <unordered_map>
+
+
+// Implementation with strings, passes with 0ms runtime
+#include <string>
+
+bool isValid(std::string s) {
+    std::string stack;
+
+    for(const auto& c : s){
+        if (stack.size() != 0 && ((c == '}' && stack.back() == '{') || (c == ']' && stack.back() == '[') || (c == ')' && stack.back() == '('))){
+            stack.pop_back();
+        }
+        else{
+            stack.push_back(c);
+        }
+    }
+    return stack.empty();
+}
+
+//Implementation with stack class, , passes with 0ms runtime
+#include <stack>
+
+bool isValid(std::string s) {
+    std::stack<char> stk;
+
+    for(const auto& c : s){
+        if (stk.size() != 0 && ((c == '}' && stk.top() == '{') || (c == ']' && stk.top() == '[') || (c == ')' && stk.top() == '('))){
+            stk.pop();
+        }
+        else{
+            stk.push(c);
+        }
+    }
+    return stk.empty();
+}
