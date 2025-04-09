a3539015694476ace9e8977ad0ad1a4575909954
Michael Akzam <michaelakzam@gmail.com>
2025-04-09 07:52:43 -0400

    Solved Sorted Two Sum 2 optimally using a 2-pointer approach

---
+++ Python/Two Pointers/Medium/TwoSum2_InputArrayIsSorted.py
@@ -0,0 +1,34 @@
+# Intuition
+'''
+'''
+
+# Approach
+'''
+'''
+
+# Complexity
+
+# - Time complexity:
+'''
+'''
+
+# - Space complexity:
+'''
+'''
+
+from typing import List
+# Two Pointer Approach
+def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left, right = 0, len(numbers)-1
+
+        while left < right:
+            if (numbers[left] + numbers[right]) == target:
+                return [left+1, right+1]
+            
+            if (numbers[left] + numbers[right]) < target:
+                left += 1
+
+            if (numbers[left] + numbers[right]) > target:
+                right -= 1
+
+        return []
