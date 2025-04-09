11b0574e50fd7e06aa128f3c8306f960e63a29f7
Michael Akzam <michaelakzam@gmail.com>
2025-04-08 21:25:16 -0400

    Solved optimally topKFrequentElements in cpp with 0ms runtime and beating 100.00%

---
+++ Cpp/Arrays/Medium/TopKFrequentElements.cpp
@@ -0,0 +1,38 @@
+/*
+Intuition
+
+
+Approach
+
+
+Complexity
+- Time complexity:
+
+- Space complexity:
+
+*/
+
+#include <queue>
+#include <unordered_map>
+
+std::vector<int> topKFrequent(std::vector<int>& nums, int k) {
+    std::unordered_map<int, int> map;
+    std::priority_queue<std::pair<int, int>> pq;
+    std::vector<int> res;
+
+    for(int i = 0; i < nums.size(); i++){
+        map[nums[i]] += 1;
+    }
+
+    for(const auto& p : map){
+        pq.push(std::make_pair(p.second, p.first));
+    }
+
+    while(k > 0){
+        int num = pq.top().second;
+        res.push_back(num);
+        pq.pop();
+        k--;
+    }
+    return res;
+}
