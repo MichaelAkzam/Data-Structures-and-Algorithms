a88ceec70e2d51727f4a66fe26d78d8d42829b7e
Michael Akzam <michaelakzam@gmail.com>
2025-04-13 21:29:46 -0400

    Optimal code for Valid Palindrome in cpp

--- Cpp/Two Pointers/Easy/ValidPalindrome.cpp
+++ Cpp/Two Pointers/Easy/ValidPalindrome.cpp
@@ -11,4 +11,25 @@ Complexity
 - Space complexity:
 
 */
+#include <string>
 
+bool isPalindrome(std::string s) {
+    int left = 0;
+    int right = s.size() - 1;
+
+    while(left < right){
+        while(!isalnum(s[left]) && left < right){
+            left++;
+        }
+        while(!isalnum(s[right]) && left < right){
+            right--;
+        }
+
+        if(tolower(s[left]) != tolower(s[right])){
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
+}
