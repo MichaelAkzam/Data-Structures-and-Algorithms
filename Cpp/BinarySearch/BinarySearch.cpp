d9ede96a4277531b05b52e064efb5800e53bb131
Michael Akzam <michaelakzam@gmail.com>
2025-05-20 10:44:44 -0400

    Solved Binary Search optimally in cpp

---
+++ Cpp/BinarySearch/BinarySearch.cpp
@@ -0,0 +1,29 @@
+/*
+Intuition
+
+
+Approach
+
+
+Complexity
+- Time complexity:
+
+- Space complexity:
+
+*/
+#include <vector>
+
+int search(std::vector<int>& nums, int target) {
+    int left = 0, right = nums.size()-1;
+
+    while(left <= right){
+        int mid = (left + right) / 2;
+        if(nums[mid] == target)
+            return mid;
+        if(nums[mid] < target)
+            left = mid + 1;
+        else if(nums[mid] > target)
+            right = mid - 1;
+    }
+    return -1;
+}
